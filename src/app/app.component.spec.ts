c26d274cf11e9aaf2c514d1bc06c5f029c9ed75e
Nosf <n0sf@mail.ru>
2019-02-21 22:13:16 +0300

    Fixed test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,20 +3,19 @@ import { AppComponent } from './app.component';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { initialState } from './store/reducers/data.reducer';
 import { Store } from '@ngrx/store';
-import { RouterModule, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
     let mockStore: MockStore<{ count: number }>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [RouterModule],
+            imports: [RouterTestingModule],
             declarations: [AppComponent],
             providers: [
                 provideMockStore({
                     initialState
                 }),
-                Router,
             ]
         }).compileComponents();
 
