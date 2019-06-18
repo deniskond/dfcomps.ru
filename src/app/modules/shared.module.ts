b3cbe2b5ddc6a0aa7ec0935786d82dfb0e21deaf
Denis Kondarattsev <deniskond@yandex.ru>
2019-06-18 23:39:33 +0300

    Markup for offline cup start

--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RatingChangeComponent } from '../components/rating-change/rating-change.component';
 import { PlayerCellComponent } from '../components/player-cell/player-cell.component';
+import { MatRippleModule } from '@angular/material';
+import { WeaponsComponent } from '../components/weapons/weapons.component';
 
-const COMPONENTS = [FlagComponent, RatingChangeComponent, PlayerCellComponent];
+const COMPONENTS = [FlagComponent, RatingChangeComponent, PlayerCellComponent, WeaponsComponent];
 
 @NgModule({
     declarations: COMPONENTS,
-    imports: [CommonModule],
+    imports: [CommonModule, MatRippleModule],
     exports: COMPONENTS,
 })
 export class SharedModule {}
--- src/app/pages/main/components/news-offline-start/news-offline-start.component.ts
+++ src/app/pages/main/components/news-offline-start/news-offline-start.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { NewsOfflineStartInterface } from '../../../../services/news-service/interfaces/news-offline-start.interface';
+import { Component, Input } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
     selector: 'app-news-offline-start',
     templateUrl: './news-offline-start.component.html',
     styleUrls: ['./news-offline-start.component.less'],
 })
-export class NewsOfflineStartComponent {}
+export class NewsOfflineStartComponent {
+    @Input()
+    news: NewsOfflineStartInterface;
+
+    public getFormattedDateTime(timestamp: string): string {
+        return moment(+timestamp * 1000).format('DD.MM.YYYY HH:mm');
+    }
+}
