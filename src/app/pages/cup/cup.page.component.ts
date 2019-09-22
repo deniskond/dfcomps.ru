01caf5752daead8a5b5b421da40a8e199a20d107
Denis Kondarattsev <deniskond@yandex.ru>
2019-09-22 20:28:44 +0300

    Added API urls

--- src/app/pages/cup/cup.page.component.ts
+++ src/app/pages/cup/cup.page.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CupPageTypes } from './enums/cup-page-types.enum';
 
 @Component({
     selector: 'app-cup-page',
     templateUrl: './cup.page.component.html',
     styleUrls: ['./cup.page.component.less'],
 })
-export class CupPageComponent {}
+export class CupPageComponent {
+    public type: CupPageTypes;
+
+    constructor(private activatedRoute: ActivatedRoute) {
+        this.type = this.activatedRoute.snapshot.params.type;
+    }
+}
