1a1df113b9c0be1f53ea61f6d9ab3d21942ee772
Denis Kondarattsev <deniskond@yandex.ru>
2019-10-29 23:42:56 +0300

    Adding custom html in news

--- src/app/pages/main/components/html-news/html-news.component.ts
+++ src/app/pages/main/components/html-news/html-news.component.ts
@@ -2,7 +2,7 @@ import { LanguageService } from '../../../../services/language/language.service'
 import { Translations } from '../../../../components/translations/translations.component';
 import { NewsInterfaceUnion } from '../../../../types/news-union.type';
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Languages } from '../../../../enums/languages.enum';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -15,7 +15,7 @@ import { takeUntil } from 'rxjs/operators';
 export class HtmlNewsComponent extends Translations implements OnInit, OnDestroy {
     @Input() news: NewsInterfaceUnion;
 
-    public newsHtml: string;
+    public newsHtml: SafeHtml;
 
     private onDestroy$ = new Subject<void>();
 
@@ -24,10 +24,14 @@ export class HtmlNewsComponent extends Translations implements OnInit, OnDestroy
     }
 
     ngOnInit(): void {
-        this.languageService.getLanguage$().pipe(takeUntil(this.onDestroy$)).subscribe((language: Languages) => {
-            this.newsHtml = language === Languages.RU ? this.news.text : this.news.textEn;
-            this.domSanitizer.bypassSecurityTrustHtml(this.newsHtml);
-        });
+        this.languageService
+            .getLanguage$()
+            .pipe(takeUntil(this.onDestroy$))
+            .subscribe((language: Languages) => {
+                const newsHtml = language === Languages.RU ? this.news.text : this.news.textEn;
+
+                this.newsHtml = this.domSanitizer.bypassSecurityTrustHtml(newsHtml);
+            });
 
         super.ngOnInit();
     }
--- src/app/pages/main/components/news-simple/news-simple.component.ts
+++ src/app/pages/main/components/news-simple/news-simple.component.ts
@@ -1,18 +1,11 @@
-import { DomSanitizer } from '@angular/platform-browser';
 import { NewsSimpleInterface } from '../../../../services/news-service/interfaces/news-simple.interface';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-news-simple',
     templateUrl: './news-simple.component.html',
     styleUrls: ['./news-simple.component.less'],
 })
-export class NewsSimpleComponent implements OnInit {
+export class NewsSimpleComponent {
     @Input() news: NewsSimpleInterface;
-
-    constructor(private domSanitizer: DomSanitizer) {}
-
-    ngOnInit(): void {
-        this.domSanitizer.bypassSecurityTrustHtml(this.news.text);
-    }
 }
