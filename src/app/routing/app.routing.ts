05f049c1603c37978476229bf2e5b7f723c9f361
deniskond <deniskond@yandex.ru>
2020-02-23 10:21:51 +0300

    feat(routing): added reflex route

--- src/app/routing/app.routing.ts
+++ src/app/routing/app.routing.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { ReflexComponent } from '../pages/reflex/reflex.component';
 
 export const appRoutes: Routes = [
     { path: '', loadChildren: './upper-routing-modules/main-site/main-site.module#MainSiteModule' },
     { path: 'cup', loadChildren: './upper-routing-modules/cup/cup.module#CupModule' },
+    { path: 'reflex', component: ReflexComponent },
 ];
