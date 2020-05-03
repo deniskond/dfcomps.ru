9e77cce9b968d215147938c13393129f3ba4ca58
Denis Kondarattsev <deniskond@yandex.ru>
2020-05-03 16:05:49 +0300

    refactor(social-links): fixed markup and observables

--- src/app/pages/main/components/news-social-links/news-social-links.component.ts
+++ src/app/pages/main/components/news-social-links/news-social-links.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 import { NewsInterfaceUnion } from '../../../../types/news-union.type';
 import { Translations } from '../../../../components/translations/translations.component';
 import { LanguageService } from '../../../../services/language/language.service';
-import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Subject, Observable } from 'rxjs';
 import { Languages } from '../../../../enums/languages.enum';
 import { MAIN_URL } from '../../../../configs/url-params.config';
 
@@ -16,32 +16,35 @@ import { MAIN_URL } from '../../../../configs/url-params.config';
 export class NewsSocialLinksComponent extends Translations implements OnInit, OnDestroy {
     @Input() news: NewsInterfaceUnion;
 
-    public language: Languages;   
-    public telegramShareLink: string;
-    public twitterShareLink: string;
+    public telegramShareLink$: Observable<string>;
+    public twitterShareLink$: Observable<string>;
     private onDestroy$ = new Subject<void>();
 
-    constructor(protected languageService: LanguageService, private changeDetectorRef: ChangeDetectorRef) {        
+    constructor(protected languageService: LanguageService) {
         super(languageService);
     }
 
     ngOnInit(): void {
-        this.languageService
-            .getLanguage$()
-            .pipe(takeUntil(this.onDestroy$))
-            .subscribe((language: Languages) => {                
-                const header = language === Languages.EN ? this.news.headerEn : this.news.header;
-                
-                this.language = language;
-                this.telegramShareLink = `https://t.me/share/url?url=${MAIN_URL}news/${this.news.id}&text=${encodeURIComponent(header)}`;
-                this.twitterShareLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(header)} ${MAIN_URL}/news/${this.news.id}`;
-                this.changeDetectorRef.detectChanges();
-            });
+        this.initLinkObservables();
     }
 
     ngOnDestroy(): void {
         this.onDestroy$.next();
         this.onDestroy$.complete();
         super.ngOnDestroy();
+    }
+
+    private initLinkObservables(): void {
+        const mappedHeader$ = this.languageService
+            .getLanguage$()
+            .pipe(map((language: Languages) => (language === Languages.EN ? this.news.headerEn : this.news.header)));
+
+        this.telegramShareLink$ = mappedHeader$.pipe(
+            map((header: string) => `https://t.me/share/url?url=${MAIN_URL}news/${this.news.id}&text=${encodeURIComponent(header)}`),
+        );
+
+        this.twitterShareLink$ = mappedHeader$.pipe(
+            map((header: string) => `https://twitter.com/intent/tweet?text=${encodeURIComponent(header)} ${MAIN_URL}/news/${this.news.id}`),
+        );
     }
 }
