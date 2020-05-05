35cca6803df647c67fadf28d7d1e4f9f51fb88f4
Denis Kondarattsev <deniskond@yandex.ru>
2020-05-05 15:48:32 +0300

    fix(invoke-function): fixed order

--- src/app/pipe/invoke-function.pipe.ts
+++ src/app/pipe/invoke-function.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class InvokeFunctionPipe implements PipeTransform {
     transform(target: any, func: (...args: any) => any, additionalArgs?: any): any {
-        return additionalArgs ? func(target) : func(target, additionalArgs);
+        return additionalArgs ? func(target, additionalArgs) : func(target);
     }
 }
