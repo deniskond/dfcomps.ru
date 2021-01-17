feb4bab7082bbe003c2e4a4a9c38a6ff509438bb
Denis <deniskond@yandex.ru>
2021-01-17 22:19:30 +0300

    feat(backend): added env variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,5 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
+
+    console.log(process.env.DB_NAME);
+    console.log(process.env.USER);
+    console.log(process.env.PASSWORD);
 }
 bootstrap();
