42298b81ef0afd43b1636af0aa38d4a3e2b6006a
Denis <deniskond@yandex.ru>
2022-05-29 15:43:33 +0300

    feat(frontend): added custom validator for news posting

--- apps/frontend/src/pages/admin/ui/admin-add-simple-news/admin-add-simple-news.component.ts
+++ apps/frontend/src/pages/admin/ui/admin-add-simple-news/admin-add-simple-news.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'dfcomps.ru-admin-add-simple-news',
@@ -11,20 +11,41 @@ export class AdminAddSimpleNewsComponent {
   public addSimpleNewsForm = new FormGroup({
     russianTitle: new FormControl('', Validators.required),
     englishTitle: new FormControl('', Validators.required),
-    postingTime: new FormControl('', Validators.required),
+    timeOption: new FormControl('now', Validators.required),
+    postingTime: new FormControl(''),
     russianText: new FormControl('', Validators.required),
     englishText: new FormControl('', Validators.required),
-  });
+  }, this.postingTimeValidator());
 
   public submitNews(): void {
-    Object.keys(this.addSimpleNewsForm.controls).forEach((key: string) => this.addSimpleNewsForm.get(key)!.markAsDirty());
+    Object.keys(this.addSimpleNewsForm.controls).forEach((key: string) =>
+      this.addSimpleNewsForm.get(key)!.markAsDirty(),
+    );
 
     if (!this.addSimpleNewsForm.valid) {
       return;
     }
+
+    alert('ok');
   }
 
   public hasFieldError(control: AbstractControl): boolean {
     return !!control!.errors && !control!.pristine;
+  }
+
+  public postingTimeValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const timeOption: string = control.get('timeOption')!.value;
+
+      if (timeOption === 'now') {
+        return null;
+      }
+
+      if (control.get('postingTime')!.value) {
+        return null;
+      }
+
+      return { postingTimeEmpty: { value: control.get('postingTime')!.value } };
+    };
   }
 }
