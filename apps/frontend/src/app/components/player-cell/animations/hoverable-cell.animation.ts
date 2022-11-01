d7c6f408058148ee438d8064e637bcf49290988b
Denis Kondarattsev <d.kondarattsev@kazanexpress.ru>
2022-11-02 01:36:24 +0500

    feat(global): dark theme for timer block

--- apps/frontend/src/app/components/player-cell/animations/hoverable-cell.animation.ts
+++ apps/frontend/src/app/components/player-cell/animations/hoverable-cell.animation.ts
@@ -5,29 +5,31 @@ export const HOVERABLE_CELL_NORMAL_STATE = 'normal';
 export const HOVERABLE_CELL_TRANSPARENT_HOVERED_STATE = 'transparent-hovered';
 export const HOVERABLE_CELL_TRANSPARENT_NORMAL_STATE = 'transparent-normal';
 
+const isDarkTheme = true; // @TODO доделать ключ
+
 export const hoverableCellAnimation = trigger('hoveredCell', [
   state(
     HOVERABLE_CELL_HOVERED_STATE,
     style({
-      backgroundColor: '#eeeeee',
+      backgroundColor: isDarkTheme ? 'rgb(34, 36, 38)' : '#eeeeee',
     }),
   ),
   state(
     HOVERABLE_CELL_NORMAL_STATE,
     style({
-      backgroundColor: 'white',
+      backgroundColor: isDarkTheme ? 'rgb(24, 26, 27)' : 'white',
     }),
   ),
   state(
     HOVERABLE_CELL_TRANSPARENT_HOVERED_STATE,
     style({
-      backgroundColor: 'rgba(255, 255, 255, 0.4)',
+      backgroundColor: isDarkTheme ? 'rgb(24, 26, 27, 0.4)' : 'rgba(255, 255, 255, 0.4)',
     }),
   ),
   state(
     HOVERABLE_CELL_TRANSPARENT_NORMAL_STATE,
     style({
-      backgroundColor: 'rgba(255, 255, 255, 0)',
+      backgroundColor: isDarkTheme ? 'rgb(24, 26, 27, 0)' : 'rgba(255, 255, 255, 0)',
     }),
   ),
   transition(`${HOVERABLE_CELL_HOVERED_STATE} => ${HOVERABLE_CELL_NORMAL_STATE}`, [animate(500)]),
