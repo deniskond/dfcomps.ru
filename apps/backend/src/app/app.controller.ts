31a0469145c5c676e301f123020a3b5529218e3a
Denis <d.kondarattsev@kazanexpress.ru>
2023-08-04 15:08:55 +0200

    feat(backend): docker compose and npm scripts for database

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('test')
   getData() {
     return this.appService.getData();
   }
