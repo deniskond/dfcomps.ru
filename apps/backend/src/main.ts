925283fb9f09fd5065df52e9cfeb9122e641aa82
Denis <d.kondarattsev@kazanexpress.ru>
2023-09-04 21:58:14 +0200

    feat(backend): moved postgres pass into env variable

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -9,9 +9,9 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = '';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = 4001;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
