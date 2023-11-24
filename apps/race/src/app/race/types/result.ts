da1955a7b49e9a727d32075753b73e183fe04ab1
Arseniy Zelensky <arseniy.zelensky@dasha.ai>
2023-11-24 20:28:39 +0300

    feat(race): finalize pipeline & add host install script

--- apps/race/src/app/race/types/result.ts
+++ apps/race/src/app/race/types/result.ts
@@ -1,11 +1,13 @@
-export type ErrorCode = 'NotFound' | 'BadRequest' | 'DuplicateKey' | 'NotAllowed';
+export type ErrorCode = 'NotFound' | 'BadRequest' | 'DuplicateKey' | 'NotAllowed' | 'InternalError';
 
 export interface ErrorMessage {
   code: ErrorCode;
   message: string;
 }
 
-export type Result<Type> = { err: ErrorMessage; result: undefined } | { err: undefined; result: Type };
+export type ErrorType = { err: ErrorMessage; result: undefined };
+export type ResultType<Type> = { err: undefined; result: Type };
+export type Result<Type> = ErrorType | ResultType<Type>;
 export type AsyncResult<Type> = Promise<Result<Type>>;
 
 export const badRequest = (message: string): Result<never> => ({
@@ -26,3 +28,14 @@ export const duplicate = (message: string): Result<never> => ({
 });
 export const error = (err: ErrorMessage): Result<never> => ({ err, result: undefined });
 export const result = <Type>(result: Type): Result<Type> => ({ result, err: undefined });
+
+export function isErrorResult(x: any): x is ErrorType {
+  return x instanceof Object && "err" in x && x.err instanceof Object && typeof x.err.code === 'string' && typeof x.err.message === 'string';
+}
+export function isAnyResult(x: any): x is ResultType<any> {
+  return x instanceof Object && "result" in x && x.result !== undefined;
+}
+
+export function isResult(x: any): x is Result<any> {
+  return isErrorResult(x) || isAnyResult(x);
+}
