e431477484c568faf3d6468c168f15c400b4d437
Denis <d.kondarattsev@kazanexpress.ru>
2024-02-26 20:04:25 +0100

    feat(e2e): authorization test

--- apps/dfcomps-e2e/src/e2e/app.cy.ts
+++ apps/dfcomps-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,14 @@
-import { getGreeting } from '../support/app.po';
-
 describe('frontend', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome frontend');
+  it('should login correctly', () => {
+    cy.get('[data-test-id=userpanel-login-button]').click();
+    cy.get('[data-test-id=password-login-button]', { timeout: 1000 }).should('be.visible');
+    cy.get('[data-test-id=password-login-button]').click();
+    cy.get('[data-test-id=login-input]', { timeout: 1000 }).should('be.visible');
+    cy.get('[data-test-id=login-input]').type('user');
+    cy.get('[data-test-id=password-input]').type('user');
+    cy.get('[data-test-id=login-action-button]').click();
+    cy.get('[data-test-id=user-nick-text]').should('contain.text', 'user');
   });
 });
