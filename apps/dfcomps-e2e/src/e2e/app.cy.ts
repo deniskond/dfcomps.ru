63a5eabf33583b7d5902c47a3bcd21b9102d36e3
Denis <d.kondarattsev@kazanexpress.ru>
2024-02-26 22:25:48 +0100

    refactor(e2e): moved login and logout to common functions

--- apps/dfcomps-e2e/src/e2e/app.cy.ts
+++ apps/dfcomps-e2e/src/e2e/app.cy.ts
@@ -1,14 +1,11 @@
-describe('frontend', () => {
-  beforeEach(() => cy.visit('/'));
+import { UserRoles } from "@dfcomps/auth";
+import { logOut, loginAs } from "../support/app.po";
 
-  it('should login correctly', () => {
-    cy.get('[data-test-id=userpanel-login-button]').click();
-    cy.get('[data-test-id=password-login-button]', { timeout: 1000 }).should('be.visible');
-    cy.get('[data-test-id=password-login-button]').click();
-    cy.get('[data-test-id=login-input]', { timeout: 1000 }).should('be.visible');
-    cy.get('[data-test-id=login-input]').type('user');
-    cy.get('[data-test-id=password-input]').type('user');
-    cy.get('[data-test-id=login-action-button]').click();
-    cy.get('[data-test-id=user-nick-text]').should('contain.text', 'user');
+describe('authorization', () => {
+  it('should login and logout correctly', () => {
+    loginAs(UserRoles.USER);
+    cy.get('[data-test-id=user-nick-text]').should('contain.text', UserRoles.USER.toLowerCase());
+    logOut();
+    cy.get('[data-test-id=user-nick-text]').should('not.exist');
   });
 });
