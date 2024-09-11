4db5e90583802a7e26b6e07a47e13a83db89ec70
Denis Kondarattsev <deniskond2@gmail.com>
2024-09-11 16:06:27 +0200

    feat(backend): changed getting levelshot logic to local

--- apps/backend/src/shared/helpers/get-map-levelshot.ts
+++ apps/backend/src/shared/helpers/get-map-levelshot.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 
 export function getMapLevelshot(mapname: string): string {
-  return fs.existsSync(`./uploads/images/maps/${mapname}.jpg`)
-    ? `./uploads/images/maps/${mapname}.jpg`
-    : `https://ws.q3df.org/images/levelshots/512x384/${mapname}.jpg`;
+  return fs.existsSync(process.env.DFCOMPS_FILES_ABSOLUTE_PATH + `/images/maps/${mapname}.jpg`)
+    ? `https://dfcomps.ru/uploads/images/maps/${mapname}.jpg`
+    : `https://dfcomps.ru/uploads/images/maps/no-levelshot.jpg`;
 }
