48098175c944552a68db35c1d7942e75e5c7ed49
Denis Kondarattsev <deniskond2@gmail.com>
2025-02-17 17:42:26 +0100

    Improved logging for demo operations

--- apps/backend/src/shared/services/logger.service.ts
+++ apps/backend/src/shared/services/logger.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Logtail } from '@logtail/node';
 
 @Injectable()
 export class LoggerService {
-  // private logger: winston.Logger;
-  // constructor() {}
-  // public log(message: string) {
-  //   this.logger.info(message);
-  // }
+  private logger: Logtail;
+
+  constructor() {
+    if (process.env.LOGTAIL_API_KEY) {
+      this.logger = new Logtail(process.env.LOGTAIL_API_KEY, {
+        endpoint: 'https://s1207863.eu-nbg-2.betterstackdata.com',
+      });
+    }
+  }
+
+  public info(message: string) {
+    if (this.logger) {
+      this.logger.info(message);
+      this.logger.flush();
+    }
+  }
+
+  public error(message: string) {
+    if (this.logger) {
+      this.logger.error(message);
+      this.logger.flush();
+    }
+  }
 }
