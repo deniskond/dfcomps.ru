ad7037466605c52b0839084a7b47fcbf6f5fecd6
Denis Kondarattsev <deniskond2@gmail.com>
2025-02-17 16:42:49 +0100

    Removing papertail logger

--- apps/backend/src/shared/services/logger.service.ts
+++ apps/backend/src/shared/services/logger.service.ts
@@ -1,29 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import * as winston from 'winston';
-import * as os from 'os';
-require('winston-syslog').Syslog;
 
 @Injectable()
 export class LoggerService {
-  private logger: winston.Logger;
-
-  constructor() {
-    this.logger = winston.createLogger({
-      format: winston.format.simple(),
-      levels: winston.config.syslog.levels,
-      transports: [
-        new (winston.transports as any).Syslog({
-          host: 'logs3.papertrailapp.com',
-          port: 39258,
-          protocol: 'tls4',
-          localhost: os.hostname(),
-          eol: '\n',
-        }),
-      ],
-    });
-  }
-
-  public log(message: string) {
-    this.logger.info(message);
-  }
+  // private logger: winston.Logger;
+  // constructor() {}
+  // public log(message: string) {
+  //   this.logger.info(message);
+  // }
 }
