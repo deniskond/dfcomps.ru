80a1a9ee98e08458d06a77d6ac1570828e9f6d34
Denis Kondarattsev <deniskond2@gmail.com>
2025-09-21 15:55:56 +0200

    fix(backend): switching levelshots back to ws

--- apps/backend/src/shared/helpers/get-map-levelshot.ts
+++ apps/backend/src/shared/helpers/get-map-levelshot.ts
@@ -3,5 +3,5 @@ import * as fs from 'fs';
 export function getMapLevelshot(mapname: string): string {
   return fs.existsSync(process.env.DFCOMPS_FILES_ABSOLUTE_PATH + `/images/maps/${mapname}.jpg`)
     ? `https://dfcomps.ru/uploads/images/maps/${mapname}.jpg`
-    : `https://dfcomps.ru/uploads/images/maps/no-levelshot.jpg`;
+    : `https://ws.q3df.org/images/levelshots/512x384/${mapname}.jpg`;
 }
