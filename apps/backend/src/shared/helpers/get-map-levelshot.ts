51da256d0103500499740f2cf11a017012682e29
Denis Kondarattsev <deniskond2@gmail.com>
2026-01-16 09:58:01 +0100

    fix(backend): relative paths for levelshots

--- apps/backend/src/shared/helpers/get-map-levelshot.ts
+++ apps/backend/src/shared/helpers/get-map-levelshot.ts
@@ -2,6 +2,6 @@ import * as fs from 'fs';
 
 export function getMapLevelshot(mapname: string): string {
   return fs.existsSync(process.env.DFCOMPS_FILES_ABSOLUTE_PATH + `/images/maps/${mapname}.jpg`)
-    ? `https://dfcomps.ru/uploads/images/maps/${mapname}.jpg`
+    ? `/uploads/images/maps/${mapname}.jpg`
     : `https://ws.q3df.org/images/levelshots/512x384/${mapname}.jpg`;
 }
